dfb3d66f58696adf9f4707142b13b055cc70340f
nguyenvukhang <brew4k@gmail.com>
2024-04-24 22:59:18 +0800

    Update Q update in householder

--- src/na/qr_decomp.rs
+++ src/na/qr_decomp.rs
@@ -6,9 +6,8 @@ pub fn householder<const M: usize, const N: usize>(
     A: &Mat<M, N>,
 ) -> (Mat<M, M>, Mat<M, N>) {
     let mut R = A.clone();
-    let (m, n) = R.dimensions();
 
-    assert!(m >= n, "Householder QR requires nrows ≥ ncols");
+    assert!(M >= N, "Householder QR requires nrows ≥ ncols");
 
     let I = Mat::eye();
 
@@ -17,28 +16,32 @@ pub fn householder<const M: usize, const N: usize>(
     for j in 1..=N {
         let s = R[(j, j)].signum();
 
-        let mut R_j = R.col(j).clone();
+        let mut v = R.col(j).clone();
 
-        (1..j).for_each(|x| R_j[x] = 0.); // zero-out entries before j.
-        let nom = R_j.l2_norm();
+        (1..j).for_each(|x| v[x] = 0.); // zero-out entries before j.
+        let nom = v.l2_norm();
+        let s_nom = s * nom;
 
-        let u = &R_j + s * nom * I.col(j);
+        // at s_nom times the j-th canonical basis vec to u.
+        v[j] += s_nom;
 
         // Forcefully set the known column
-        R[(j, j)] = -s * nom;
-        (j + 1..m + 1).for_each(|i| R[(i, j)] = 0.);
+        R[(j, j)] = -s_nom;
+        (j + 1..=M).for_each(|i| R[(i, j)] = 0.);
 
-        let v = &u / u.l2_norm();
+        v.l2_normalize();
         let vt = v.t();
 
         // Rpply the HH transform on the remaining columns.
-        for i in j + 1..n + 1 {
+        for i in j + 1..=N {
             let x = R.col(i);
             let sub = x - 2. * &v * (&vt * x);
-            (j..m + 1).for_each(|k| R[(k, i)] = sub[(k, 1)]);
+            (j..=M).for_each(|k| R[(k, i)] = sub[(k, 1)]);
         }
 
-        Q = Q * (&I - 2. * v * vt);
+        // Q = Q * (I - 2vvᵀ);
+        let B = &Q * 2. * v * vt;
+        Q = Q - B;
     }
 
     (Q, R)
